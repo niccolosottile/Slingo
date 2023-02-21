42387a9ce32cf0a8daab285f105fba513c82e85d
= <niccolosottile@outlook.com>
2023-02-21 19:56:52 +0000

    Added the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,13 @@ import RecordList from "./components/recordList";
 import Edit from "./components/edit";
 import Create from "./components/create";
 
+import Home from "./components/home";
+import Login from "./components/login";
+import Signup from "./components/signup";
+import InHome from "./components/inHome"; 
+import Quiz from "./components/quiz";
+import Translate from "./components/translate";
+
 const App = () => {
     return (
         <div>
@@ -17,6 +24,12 @@ const App = () => {
                 <Route exact path="/" element={<RecordList />} />
                 <Route path="/edit/:id" element={<Edit />} />
                 <Route path="/create" element={<Create />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/inhome" element={<InHome />} />
+                <Route path="/quiz" element={<Quiz />} />
+                <Route path="/translate" element={<Translate />} />
             </Routes>
         </div>
     );
