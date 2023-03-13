92319f2898606e226c41bab4b406692a8b0a35a4
seetvn <waiyansteven2002@gmail.com>
2023-03-13 01:26:23 +0000

    added google_id field and changed password

--- server/models/user.js
+++ server/models/user.js
@@ -6,9 +6,13 @@ const passwordComplexity = require("joi-password-complexity");
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
-    password: { type: String, required: true},
+    password: { type: String, required: false},
     verified: { type: Boolean, default: false},
+    google_id:{ type: Number,default:-1}
+    //progress:{}
 });
+
+
 
 userSchema.methods.generateAuthToken = function () {
     const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY, {
