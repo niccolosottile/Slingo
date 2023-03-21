12be8ebd4de696697a97ad6461414809ce51c203
= <niccolosottile@outlook.com>
2023-03-21 12:40:18 +0000

    Complex progress function

--- client/src/components/learnSign2.js
+++ client/src/components/learnSign2.js
@@ -18,34 +18,40 @@ import your from "./family/your.jpg";
 export default function LearnSign2() {
 	const navigate = useNavigate();
 
-	const [progress, setProgress] = useState(0);
-	const userId = localStorage.getItem("userid");
+	// Retrieving course details
+	const [loading, setLoading] = useState(true);
+	const [signs, setSigns] = useState([]);
+	const [signId, setSignId] = useState("");
 
 	useEffect(() => {
-		const retrieveProgress = async () => {
+		const retrieveSignsAndSetSign = async () => {
 			try {
-				const url = `http://localhost:8080/api/progress/${userId}`;
+				const url = `http://localhost:8080/api/courses/family`;
 				const { data: res } = await axios.get(url);
-				setProgress(res.overallProgress);
+				setSigns(res.course.signs);
+				setSignId(res.course.signs[0]._id);
+				setLoading(false);
 			} catch (error) {
 				console.log(error);
 			}
 		};
-		retrieveProgress();
+		retrieveSignsAndSetSign();
 	}, []);
 
-	const updateProgress = async () => {
-		try {
-			const url = `http://localhost:8080/api/progress/${userId}`;
-			const newProgress = progress + 1;
-			await axios.post(url, { newProgress });
-			setProgress(newProgress);
-		} catch (error) {
-			console.log(error);
-		}
-	};
-
+	const userId = localStorage.getItem("userid");
 	const [currentIndex, setCurrentIndex] = useState(0);
+	
+	useEffect(() => {
+		const updateProgress = async () => {
+			try {
+				const url = `http://localhost:8080/api/progress/${userId}/${signId}`;
+				await axios.post(url);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		updateProgress();
+	}, [currentIndex]);
 
 	const images = [
 		{ src: father, alt: "Father" },
@@ -61,103 +67,50 @@ export default function LearnSign2() {
 		{ src: your, alt: "Your" },
 	];
 
-	const signs = [
-		{
-			name: "Father",
-			description:
-				"Hold up your index finger and your middle finger on both hands and cross them over, pointing downwards.",
-		},
-		{
-			name: "Mother",
-			description:
-				"Hold up your three middle fingers on one hand and place them on the other hand's flat palm.",
-		},
-		{
-			name: "Son",
-			description:
-				"Point with your index finger to the side of you at your chin level.",
-		},
-		{
-			name: "Daughter",
-			description:
-				"Make a D shape using the index finger of one hand and the thumb and index finger of the other.",
-		},
-		{
-			name: "Brother",
-			description: "Hold two closed fists side by side in front of you.",
-		},
-		{
-			name: "Sister",
-			description:
-				"Make a curl in your index finger on one hand and hold it above your nose.",
-		},
-		{
-			name: "Step",
-			description:
-				"Touch your little finger on one hand with the other hand's index finger.",
-		},
-		{
-			name: "Baby",
-			description:
-				"Place one flat hand over another as if you are cradling a baby.",
-		},
-		{
-			name: "Home",
-			description:
-				"Hold two open flat hands at 45 degrees so the tips of them meet, like a tent.",
-		},
-		{
-			name: "My",
-			description: "Place one enclosed fist on your chest.",
-		},
-		{
-			name: "Your",
-			description:
-				"Curl over all your fingers on a flat palm and hold it out in front of you.",
-		},
-	];
-
 	function handleNextClick() {
 		const nextIndex = (currentIndex + 1) % images.length;
 		setCurrentIndex(nextIndex);
-		updateProgress();
-	};
+		setSignId(signs[nextIndex]._id);
+	}
 
 	function handlePrevClick() {
 		const prevIndex = currentIndex === 0 ? images.length - 1 : currentIndex - 1;
 		setCurrentIndex(prevIndex);
+		setSignId(signs[prevIndex]._id);
 	}
 
-	return (
-		<div className={learnSignCSS.container}>
-			<Navbar />
-			<div className={learnSignCSS.content}>
-				<h1 className={learnSignCSS.title}>{signs[currentIndex]["name"]}</h1>
-				<div className={learnSignCSS["slide-show"]}>
-					<button
-						className={learnSignCSS.previous}
-						onClick={handlePrevClick}
-					>
-						Previous
-					</button>
-					<img
-						src={images[currentIndex].src}
-						alt={images[currentIndex].alt}
-					/>
-					<button
-						className={learnSignCSS.next}
-						onClick={handleNextClick}
-					>
-						Next
+	if (!loading) {
+		return (
+			<div className={learnSignCSS.container}>
+				<Navbar />
+				<div className={learnSignCSS.content}>
+					<h1 className={learnSignCSS.title}>{signs[currentIndex]["name"]}</h1>
+					<div className={learnSignCSS["slide-show"]}>
+						<button
+							className={learnSignCSS.previous}
+							onClick={handlePrevClick}
+						>
+							Previous
+						</button>
+						<img
+							src={images[currentIndex].src}
+							alt={images[currentIndex].alt}
+						/>
+						<button
+							className={learnSignCSS.next}
+							onClick={handleNextClick}
+						>
+							Next
+						</button>
+					</div>
+					<p className={learnSignCSS.description}>
+						{signs[currentIndex]["description"]}
+					</p>
+					<button onClick={() => navigate("/learn")} className={learnSignCSS["leave-button"]}>
+						Leave session
 					</button>
 				</div>
-				<p className={learnSignCSS.description}>
-					{signs[currentIndex]["description"]}
-				</p>
-				<button onClick={() => navigate("/learn")} className={learnSignCSS["leave-button"]}>
-					Leave session
-				</button>
 			</div>
-		</div>
-	);
+		);
+	}
 }
